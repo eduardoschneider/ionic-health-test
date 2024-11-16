46f0381beb3f227e9b911b293ead445551d51ec2
Eduardo Schneider <eduardo@fidelizi.com.br>
2024-11-16 04:56:07 -0300

    [HOTFIX] Ajustando tamanho das colunas das informações

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -40,96 +40,107 @@ const Details: React.FC = () => {
             <img src={item?.thumbnail?.path + '.' + item?.thumbnail?.extension}></img>
             <span className="name">{item?.name}</span>
             <span className="description">{item?.description}</span>
-            <a className="next">{item?.next?.name}</a>
-            <a className="previous">{item?.previous?.name}</a>
+
+            {item?.next &&
+              <a className="navigate next">
+                <span>Próximo</span><br />
+                {item?.next?.name}
+              </a>
+            }
+            {item?.previous &&
+              <a className="navigate previous">
+                <span>Anterior</span><br />
+                {item?.previous?.name}
+              </a>
+            }
 
             <span className="modified">
               <a>Última modificação</a> <br />
               {format(parseISO(item.modified), 'dd/MM/yyyy HH:mm')}
             </span>
           </div>
-          
+
           <div className="content">
 
-            { item?.comics &&
-            <div className="block">
-              <span className="title">Aparece nas seguintes comics:</span>
-              <div className="block-content">
-                {
-                  item?.comics.items.map((item:any, index:number) => {
-                    return <span key={index} className="card-content comic">{item.name}</span>
-                  })
-                }
+            {item?.comics &&
+              <div className="block">
+                <span className="title">Aparece nas seguintes comics:</span>
+                <div className="block-content">
+                  {
+                    item?.comics.items.map((item: any, index: number) => {
+                      return <span key={index} className="card-content comic">{item.name}</span>
+                    })
+                  }
+                </div>
               </div>
-            </div>
             }
 
-            { item?.stories &&
-            <div className="block">
-              <span className="title">Aparece nas seguintes histórias:</span>
-              <div className="block-content">
-                {
-                  item?.stories.items.map((item:any, index:number) => {
-                    return <span key={index} className="card-content stories">{item.name}</span>
-                  })
-                }
+            {item?.stories &&
+              <div className="block">
+                <span className="title">Aparece nas seguintes histórias:</span>
+                <div className="block-content">
+                  {
+                    item?.stories.items.map((item: any, index: number) => {
+                      return <span key={index} className="card-content stories">{item.name}</span>
+                    })
+                  }
+                </div>
               </div>
-            </div>
             }
 
-            { item?.events &&
-            <div className="block">
-              <span className="title">Aparece nos seguintes eventos:</span>
-              <div className="block-content">
-                {
-                  item?.events?.items.map((item:any, index:number) => {
-                    return <span key={index} className="card-content events">{item.name}</span>
-                  })
-                }
+            {item?.events &&
+              <div className="block">
+                <span className="title">Aparece nos seguintes eventos:</span>
+                <div className="block-content">
+                  {
+                    item?.events?.items.map((item: any, index: number) => {
+                      return <span key={index} className="card-content events">{item.name}</span>
+                    })
+                  }
+                </div>
               </div>
-            </div>
             }
 
-            { item?.characters &&
-            <div className="block">
-              <span className="title">Aparece os seguintes personagens:</span>
-              <div className="block-content">
-                {
-                  item?.characters?.items.map((item:any, index:number) => {
-                    return <span key={index} className="card-content events">{item.name}</span>
-                  })
-                }
+            {item?.characters &&
+              <div className="block">
+                <span className="title">Aparece os seguintes personagens:</span>
+                <div className="block-content">
+                  {
+                    item?.characters?.items.map((item: any, index: number) => {
+                      return <span key={index} className="card-content events">{item.name}</span>
+                    })
+                  }
+                </div>
               </div>
-            </div>
             }
 
-            { item?.series &&
-            <div className="block">
-              <span className="title">Aparece nas seguintes séries:</span>
-              <div className="block-content">
-                {
-                  item?.series.items.map((item:any, index:number) => {
-                    return <span key={index} className="card-content series">{item.name}</span>
-                  })
-                }
+            {item?.series &&
+              <div className="block">
+                <span className="title">Aparece nas seguintes séries:</span>
+                <div className="block-content">
+                  {
+                    item?.series.items.map((item: any, index: number) => {
+                      return <span key={index} className="card-content series">{item.name}</span>
+                    })
+                  }
+                </div>
               </div>
-            </div>
             }
 
-            
-            { item?.creators &&
-            <div className="block">
-              <span className="title">Criadores:</span>
-              <div className="block-content">
-                {
-                  item?.creators.items.map((item:any, index:number) => {
-                    return <span key={index} className="card-content creators">{item.name}</span>
-                  })
-                }
+
+            {item?.creators &&
+              <div className="block">
+                <span className="title">Criadores:</span>
+                <div className="block-content">
+                  {
+                    item?.creators.items.map((item: any, index: number) => {
+                      return <span key={index} className="card-content creators">{item.name}</span>
+                    })
+                  }
+                </div>
               </div>
-            </div>
             }
-            
+
           </div>
         </>
       }
